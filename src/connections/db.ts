e25667d7dd15b27d7c66389a5d5cdcd9a9cb94e8
Chirag Katrodiya <chirag.katrodiya@solulab.co>
2024-03-17 01:25:16 +0530

    db
    Signed-off-by: Chirag Katrodiya <chirag.katrodiya@solulab.co>

--- src/connections/db.ts
+++ src/connections/db.ts
@@ -14,40 +14,10 @@ const _password: string = Config.DB.DB_PASSWORD!
 const _host: string = Config.DB.DB_HOST!
 const _port: string = Config.DB.DB_PORT!
 
-// const db = new Sequelize(
-// 	// 'postgres://utsavdb_user:9vXYrUOnQ1a3Rr7PAXozMJYCyBXBOAw9@dpg-cls621vqd2ns73e0i2l0-a/utsavdb?sslmode=no-verify',
-// 	'postgres://utsavdb_user:9vXYrUOnQ1a3Rr7PAXozMJYCyBXBOAw9@dpg-cls621vqd2ns73e0i2l0-a.oregon-postgres.render.com/utsavdb_5bfy?sslmode=no-verify',
-// 	{
-// 		storage: ':memory:',
-// 		logging: (msg) => Logger.info(msg),
-// 		models: [user, SatsangProfile, SamparkVrund, Karykarm, FollowUp],
-// 		define: {
-// 			freezeTableName: true,
-// 		},
-// 		retry: {
-// 			max: Infinity,
-// 			match: [
-// 				/ConnectionError/,
-// 				/SequelizeConnectionError/,
-// 				/SequelizeConnectionRefusedError/,
-// 				/SequelizeHostNotFoundError/,
-// 				/SequelizeHostNotReachableError/,
-// 				/SequelizeInvalidConnectionError/,
-// 				/SequelizeConnectionTimedOutError/,
-// 				/SequelizeConnectionAcquireTimeoutError/,
-// 				/Connection terminated unexpectedly/,
-// 			],
-// 		},
-// 	}
-// )
-
 const db = new Sequelize(
+	// 'postgres://utsavdb_user:9vXYrUOnQ1a3Rr7PAXozMJYCyBXBOAw9@dpg-cls621vqd2ns73e0i2l0-a/utsavdb?sslmode=no-verify',
+	'postgres://utsavdb_user:9vXYrUOnQ1a3Rr7PAXozMJYCyBXBOAw9@dpg-cls621vqd2ns73e0i2l0-a.oregon-postgres.render.com/utsavdb_5bfy?sslmode=no-verify',
 	{
-		database:_database,
-		dialect:"postgres",
-		username: _username,
-		password:_password,
-		port: 5433,
 		storage: ':memory:',
 		logging: (msg) => Logger.info(msg),
 		models: [user, SatsangProfile, SamparkVrund, Karykarm, FollowUp],
@@ -70,5 +40,35 @@ const db = new Sequelize(
 		},
 	}
 )
+
+// const db = new Sequelize(
+// 	{
+// 		database:_database,
+// 		dialect:"postgres",
+// 		username: _username,
+// 		password:_password,
+// 		port: 5433,
+// 		storage: ':memory:',
+// 		logging: (msg) => Logger.info(msg),
+// 		models: [user, SatsangProfile, SamparkVrund, Karykarm, FollowUp],
+// 		define: {
+// 			freezeTableName: true,
+// 		},
+// 		retry: {
+// 			max: Infinity,
+// 			match: [
+// 				/ConnectionError/,
+// 				/SequelizeConnectionError/,
+// 				/SequelizeConnectionRefusedError/,
+// 				/SequelizeHostNotFoundError/,
+// 				/SequelizeHostNotReachableError/,
+// 				/SequelizeInvalidConnectionError/,
+// 				/SequelizeConnectionTimedOutError/,
+// 				/SequelizeConnectionAcquireTimeoutError/,
+// 				/Connection terminated unexpectedly/,
+// 			],
+// 		},
+// 	}
+// )
 
 export default db
