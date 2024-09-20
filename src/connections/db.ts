fe44bb3b4ecda4326b8f635090078988fb8cb521
chirag188 <chirag.katrodiya@solulab.co>
2024-09-20 22:33:27 +0530

    db upgrade
    Signed-off-by: chirag188 <chirag.katrodiya@solulab.co>

--- src/connections/db.ts
+++ src/connections/db.ts
@@ -16,7 +16,8 @@ const _password: string = Config.DB.DB_PASSWORD!
 
 const db = new Sequelize(
 	// 'postgres://utsav:3kdDqB5XIqr8AGFE5V1nBf3B5LvInAb0@dpg-cpnb6vg8fa8c73atvq20-a/utsav_db_sfad?sslmode=no-verify',
-	"postgres://utsavdb_zxzg_user:KNHbBj7axeE91VUEPWLN1LR8f04OImCS@dpg-cr2ronrv2p9s739cirv0-a.oregon-postgres.render.com/utsavdb_zxzg?sslmode=no-verify",
+	"postgres://utsavdb:nNvNsKSSwtPXmFwLLwfuNa4WLqtwx96U@dpg-crmqkhlumphs739jbam0-a.oregon-postgres.render.com/utsavdb_pbir?sslmode=no-verify",
+	// "postgres://utsavdb_zxzg_user:KNHbBj7axeE91VUEPWLN1LR8f04OImCS@dpg-cr2ronrv2p9s739cirv0-a.oregon-postgres.render.com/utsavdb_zxzg?sslmode=no-verify",
 	{
 		storage: ':memory:',
 		logging: (msg) => Logger.info(msg),
